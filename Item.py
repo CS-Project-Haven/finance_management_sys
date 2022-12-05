43e0b5c3522c9fb578cf4eccc1783d9597ab746b
SPARTAN-042 <sentinal042@gmail.com>
2022-12-05 04:18:58 +0000

    additions to item and user entities. db file reinstated

--- Item.py
+++ Item.py
@@ -2,10 +2,10 @@
 
 class Item:
 
-    def __init__(self, item_id, item_name, category_name, item_price, timestamp):
+    def __init__(self, item_id, item_name, category_name, item_price, date):
         self.id = item_id
         self.item_name = item_name
         self.category_name = category_name
         self.item_price = item_price
-        self.timestamp = timestamp
+        self.date = date
 
--- User.py
+++ User.py
@@ -2,6 +2,8 @@
 
 class User:
 
-    def __init__(self, name, age):
+    def __init__(self, user_id, name, age, balance):
+        self.id = user_id
         self.name = name
         self.age = age
+        self.balance = balance
